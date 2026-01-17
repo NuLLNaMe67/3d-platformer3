3bebc00236ff3f9bd4f68aa2fffdf46fd240f6cc
NuLLNaMe67 <nullnameapple@yandex.ru>
2026-01-17 20:10:09 +0800

    Z C

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speedFactor = 2.5f;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        //ну скорость бега
+        other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+
+       other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+       
+
+    }
+}
